0fb027e8ab7fdec1417d4349e935fe6931d96720
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-21 04:09:03 +0530

    Create Maximum Value of X.java

---
+++ weekly Contest 09-07-22/Maximum Value of X.java
@@ -0,0 +1,56 @@
+/*
+Maximum Value of X
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+You are given an array A of size N, and an integer x = 0. In one move, you can choose two indices i and j such that i < j and add the maximum of these integers from the two to x. And finally, delete that bigger number from the array. If both elements are equal you can remove any of the two and add that value to x.
+After performing this operation a maximum of K times, find the maximum possible value of x you can get.
+Input
+First line of the input contains two integers, N and K.
+The second line of the input contains N space seperated integers.
+
+Constraints:
+1 <= K < N <= 105
+1 <= Ai <= 109
+Output
+Print the maximum possible value of x you can get.
+Example
+Sample Input:
+5 2
+5 6 4 2 3
+
+Sample Output:
+11
+
+Explanation:
+In the first move, we choose i = 1 and j = 3. We get the max value of these as 5, and then we add it to x and remove it from the array. x = 5, A = [6, 4, 2, 3]
+In the next and final move, we again choose indices i = 1, j = 3. Updated x = 11, A = [4, 2, 3]
+*/
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int m=sc.nextInt();
+   int num[]=new int[n];
+   for(int i=0;i<n;i++){
+       num[i]=sc.nextInt();
+   }
+   Arrays.sort(num);
+   long y=0;
+   int r=num.length;
+   for(int j=r-1;j>=0;j--){
+       if(m>0){
+           m--;
+       }else
+           break;
+       y=y+num[j];
+   }
+   System.out.println(y);
+    }
+}
