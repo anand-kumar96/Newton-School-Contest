83078311dc8e334bf716d768879750971fc6e3b0
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-09-01 06:40:49 +0530

    Create Maximum string.java

---
+++ Weekly Contest 30-07-22/Maximum string.java
@@ -0,0 +1,32 @@
+/*
+Maximum string
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given a string S, and an integer K your task is to find a lexicographically largest string by removing not more than K characters.
+Input
+The first line of input contains a single string S, the next line of input contains a single integer K.
+
+Constraints:-
+1 <= |S| <= 100000
+1 <= K <= 100000
+
+Note:- String will contain only lower case English letters.
+Output
+Print the lexicographically largest string possible after removing not more than K characters.
+Example
+Sample Input:-
+abzccd
+4
+
+Sample Output:-
+zd
+
+Sample Input:-
+dcba
+5
+
+Sample Output:-
+dcba
+*/
